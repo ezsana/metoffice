dc09275a3c6917f14db0bf0b6809c4c65e010672
Zsuzsanna Erdos <ezsana@gmail.com>
2020-02-06 13:33:46 +0100

    Properties file created. After hook not working.

--- src/test/java/com/codecool/zsana/metoffice/StepDefinitions.java
+++ src/test/java/com/codecool/zsana/metoffice/StepDefinitions.java
@@ -2,13 +2,20 @@ package com.codecool.zsana.metoffice;
 
 import com.codecool.zsana.metoffice.base.KeywordRepository;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class StepDefinitions {
 
     private KeywordRepository repository = new KeywordRepository();
+
+    @Before
+    public void letsStart() {
+        System.out.println("We start here.");
+    }
 
     @Given("that I want to open met.hu")
     public void that_I_want_to_open_met_hu() {
@@ -23,6 +30,7 @@ public class StepDefinitions {
     @Then("met.hu is open")
     public void met_hu_is_open() {
         repository.navigate("http://met.hu");
+        Assert.assertTrue(repository.isElementVisible(repository.getProperties().getProperty("mainMenuPath")));
     }
 
     @After
--- src/test/java/com/codecool/zsana/metoffice/base/KeywordRepository.java
+++ src/test/java/com/codecool/zsana/metoffice/base/KeywordRepository.java
@@ -1,26 +1,60 @@
 package com.codecool.zsana.metoffice.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class KeywordRepository {
 
+    private Properties properties;
+    private FileInputStream propFile;
     private WebDriver chromeDriver;
     private Driver driver;
+    private WebDriverWait wait;
 
     // with this, I have a driver open
     public KeywordRepository() {
+        try {
+            properties = new Properties();
+            propFile = new FileInputStream("/home/zsana/Codecool/TESTAUTOMATION/metoffice/metoffice/src/test/resources/com/codecool/zsana/metoffice/metoffice.properties");
+            properties.load(propFile);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
         driver = new Driver();
         chromeDriver = driver.setupChromeDriver();
+        wait = new WebDriverWait(chromeDriver, 10);
     }
 
     public void closeDriver() {
         if (chromeDriver != null) {
+            chromeDriver.close();
             chromeDriver.quit();
         }
     }
 
     public void navigate(String url) {
         chromeDriver.navigate().to(url);
+    }
+
+    public void hoverOverElement(String elementPath) {
+
+    }
+
+    public void clickOnElement(String elementPath) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(elementPath))).click();
+    }
+
+    public boolean isElementVisible(String elementPath) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(elementPath))).isDisplayed();
+    }
+
+    public Properties getProperties() {
+        return properties;
     }
 
 }
